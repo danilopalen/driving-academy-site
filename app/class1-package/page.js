5feb335a145123e5d180bbbc70f1db029015ee23
Danilo Palen <danilo.palen@uirev.com>
2025-01-05 22:46:32 +1300

    update import to AreaModal

--- app/class1-package/page.js
+++ app/class1-package/page.js
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import AreaModal from "../_components/areaModal";
+import AreaModal from "../_components/AreaModal";
 import {
   Clock,
   CheckCircle,
